d4503f52eac9eaedcae094c04e6ba18434281479
pavankarthiktanikanti <pavankarthiktanikanti@gmail.com>
2020-01-10 12:09:17 +0530

    Dynamic Content: Rendered Subscribers Dynamically

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import Header from './Header';
 import './App.css'
 
 function App() {
+  let subscribers = [
+    {
+      name: "Shilpa",
+      phone: "8888888888"
+    },
+    {
+      name: "Srishti",
+      phone: "9999999999"
+    }
+  ]
   return (
     <div>
       <Header />
@@ -12,6 +22,16 @@ function App() {
           <span className="grid-item name-heading">Name</span>
           <span className="grid-item phone-heading">Phone</span>
         </div>
+        {
+          subscribers.map(sub => {
+            return (
+              <div className="grid-container">
+                <span className="grid-item">{sub.name}</span>
+                <span className="grid-item">{sub.phone}</span>
+              </div>
+            )
+          })
+        }
       </div>
     </div>
   );
